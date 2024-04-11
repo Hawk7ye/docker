ea667db19657647c04f9db30bf290012e3335cfd
Hawk7ye <david.fassnacht76@gmail.com>
2024-04-11 19:49:37 +0200

    created entrypoint.sh and add TZ

---
+++ laravel/bin/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Setze Standardwerte, falls die Umgebungsvariablen nicht definiert sind
+UPLOAD_MAX_FILESIZE=${UPLOAD_MAX_FILESIZE:-2M}
+POST_MAX_SIZE=${POST_MAX_SIZE:-8M}
+
+# Ersetzen von upload_max_filesize und post_max_size in php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = $UPLOAD_MAX_FILESIZE/" /etc/php/8.2/apache2/php.ini
+sed -i "s/post_max_size = .*/post_max_size = $POST_MAX_SIZE/" /etc/php/8.2/apache2/php.ini
+
+# Starte Apache im Vordergrund
+exec /usr/sbin/apache2ctl -D FOREGROUND
